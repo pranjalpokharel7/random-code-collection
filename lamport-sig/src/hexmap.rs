de76563f515d4f447272d8122ac6863411bd0b76
pranjalpokharel7 <pranjalpokharel007@gmail.com>
2024-11-30 09:47:24 +0545

    refactor: better match pattern

--- lamport-sig/src/hexmap.rs
+++ lamport-sig/src/hexmap.rs
@@ -1,21 +1,7 @@
 pub fn hex_map_u8(utf_byte: &u8) -> u8 {
     match utf_byte {
-        48 => 0,
-        49 => 1,
-        50 => 2,
-        51 => 3,
-        52 => 4,
-        53 => 5,
-        54 => 6,
-        55 => 7,
-        56 => 8,
-        57 => 9,
-        97 => 10,
-        98 => 11,
-        99 => 12,
-        100 => 13,
-        101 => 14,
-        102 => 15,
+        48..=57 => utf_byte - 48,
+        97..=102 => utf_byte - 87,
         x => panic!("Encountered unexpected hex character: {}", x),
     }
 }
